60df1e7c132e9cbd336f1984bc4a5dd91edfbcd4
rmls <rodrigomoreiralima@hotmail.com>
2023-01-10 14:45:22 -0300

    feat: criancao dos methods da interface repositories

--- src/main/java/br/com/desafio/attornatus/repository/AddressDAO.java
+++ src/main/java/br/com/desafio/attornatus/repository/AddressDAO.java
@@ -3,4 +3,8 @@ package br.com.desafio.attornatus.repository;
 import br.com.desafio.attornatus.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressDAO extends JpaRepository<Address, Long> {}
+import java.util.List;
+
+public interface AddressDAO extends JpaRepository<Address, Long> {
+    List<Address> findAddressesByPeopleId(Long personId);
+}
--- src/main/java/br/com/desafio/attornatus/repository/PersonDAO.java
+++ src/main/java/br/com/desafio/attornatus/repository/PersonDAO.java
@@ -3,4 +3,8 @@ package br.com.desafio.attornatus.repository;
 import br.com.desafio.attornatus.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonDAO extends JpaRepository<Person, Long> {}
+import java.util.List;
+
+public interface PersonDAO extends JpaRepository<Person, Long> {
+    List<Person> findPersonsByAddressesId(Long addressId);
+}
